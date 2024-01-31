974d0fa45df1a499d3930f7d5961c8a3ec57e06d
Nick Nikiforuk <goldlamborgini@gmail.com>
2024-01-31 21:37:58 +0200

    start-app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import { AppProps } from 'next/app';
+import '../styles/globals.scss';
+import Layout from '@/components/Layout/Layout';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
