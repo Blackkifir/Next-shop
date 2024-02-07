180f871f5e10bceae3584ce5989c938563df767f
Nick <goldlamborgini@gmail.com>
2024-02-07 12:58:14 +0200

    Added dynamic class homeLoader and included a loading spinner when displaying products.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,36 @@
 import Catalog from '@/components/Catalog/Catalog';
 import { IPropsItems } from '@/redux/slices/interfaces/IPropsItems';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Loader from '@/components/Loader/Loader';
 import styles from './styles/pagesStyles/Home.module.scss';
 
-export default function Home({ items }: { items: IPropsItems[] }) {
+export default function Home({
+  items,
+}:
+{ items: IPropsItems[] }) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (items.length === 0) {
+      setIsLoading(true);
+    } else {
+      setIsLoading(false);
+    }
+  }, [items.length]);
+
   return (
-    <div className={styles.homeContainer}>
-      {items.length !== 0
-          && items.map((obj) => (
-            <Catalog
-              key={obj.id}
-              id={obj.id}
-              title={obj.title}
-              price={obj.price}
-              categoryName={obj.category.name}
-              categoryImage={obj.category.image}
-            />
-          ))}
+    <div className={isLoading ? styles.homeLoader : styles.homeContainer}>
+      {isLoading ? <Loader /> : items.map((obj) => (
+        <Catalog
+          key={obj.id}
+          id={obj.id}
+          title={obj.title}
+          price={obj.price}
+          categoryName={obj.category.name}
+          categoryImage={obj.category.image}
+        />
+      ))}
     </div>
   );
 }
@@ -25,7 +39,6 @@ export async function getServerSideProps() {
   try {
     const response = await axios.get('https://api.escuelajs.co/api/v1/products?offset=0&limit=15');
     const data = response.data as IPropsItems[];
-
     return {
       props: {
         items: data,
@@ -33,7 +46,6 @@ export async function getServerSideProps() {
     };
   } catch (error) {
     console.error('Error fetching data:', error);
-
     return {
       props: {
         items: [],
